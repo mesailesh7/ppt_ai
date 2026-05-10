60c56a3d70f10a82002244aa0b87b3f8c3795b29
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-05-10 11:43:06 -0600

    Database Integration (Prisma + Postgres)

---
+++ src/lib/db.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '#/generated/prisma/client.js'
+
+import { PrismaPg } from '@prisma/adapter-pg'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+})
+
+// Global window in a session
+declare global {
+  // Global variable that can be accessed from anywhere
+  var __prisma: PrismaClient | undefined
+}
+
+export const prisma = globalThis.__prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.__prisma = prisma
+}
--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Card } from '#/components/ui/card'
 import { Switch } from '#/components/ui/switch'
+import { DropdownMenu } from '#/components/ui/dropdown-menu'
+import { Badge } from '#/components/ui/badge'
 
 export const Route = createFileRoute('/contact')({
   component: Contact,
@@ -11,6 +13,13 @@ function Contact() {
     <div>
       <Card className="w-3xl bg-amber-300">Hello "/contact"!</Card>
       <Switch />
+      <DropdownMenu />
+      <div className="flex w-full flex-wrap justify-center gap-2">
+        <Badge>Badge</Badge>
+        <Badge variant="secondary">Secondary</Badge>
+        <Badge variant="destructive">Destructive</Badge>
+        <Badge variant="outline">Outline</Badge>
+      </div>
     </div>
   )
 }
